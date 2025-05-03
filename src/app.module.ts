e97d59673c4dc9bbec416361a4e086f2da4bad20
Eric Freitas <ericfreitas371@gmail.com>
2025-05-02 20:27:13 -0400

    simplify app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
